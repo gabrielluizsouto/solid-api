b6d992a54a91c35a6717e7d90ee615368ba47431
Gabriel Souto <gabrielluizferraz@gmail.com>
2020-12-05 14:51:14 -0300

    finishing the project. without tests

---
+++ src/providers/IMailProvider.ts
@@ -0,0 +1,15 @@
+interface IAdress {
+    email: string;
+    name: string;
+}
+
+export interface IMessage {
+    to: string;
+    from: string;
+    subject: string;
+    body: string;
+}
+
+export interface IMailProvider {
+    sendMail(message: IMessage): Promise<void>;
+}
--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,11 +1,14 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
     constructor(
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider,
     ){}
+    
     async execute(data: ICreateUserRequestDTO) {
         const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
 
@@ -16,5 +19,18 @@ export class CreateUserUseCase {
         const user = new User(data);
 
         await this.usersRepository.save(user);
+
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email,
+            },
+            from: {
+                name: 'My App team',
+                email: 'equipe@meuapp.com',
+            },
+            subject: 'Welcome to  the platform',
+            body: '<p>You are already able to login in our platform.</p>'
+        })
     }
 }
